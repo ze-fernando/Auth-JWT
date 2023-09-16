a8332668e8e68763fb40b4cbb70600408865d811
ze-fernando <jfenginner935@gmail.com>
2023-09-16 16:01:02 -0300

    Insert db with login 01

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -5,23 +5,33 @@ const jwt = require('jsonwebtoken');
 const db = require('../connection/conn');
 const bcrypt = require('bcrypt');
 
+const salt = bcrypt.genSaltSync(15);
 
-route.post('/login', (req, res) => {
+route.post('/login', async (req, res) => {
     const {username, pass} = req.body;
-    const user = users.find((u) => u.username === username && u.pass === pass);
+    const hash = bcrypt.hashSync(pass, salt);
+    const sql = `SELECT * FROM users WHERE username = '${username}' AND passrowd = '${hash}'`;
+
+    try {
+        const [query] = await db.execute(sql);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({'error': 'Error internal'});
+    }
+
     
     if (!user) return res.status(401).json({"error": "Invalid keys"});
 
+    
     const token = jwt.sign({ username: user.username },
         secretKey, {expiresIn: '30min'});
 
-    res.json({token});
+    res.status(200).json({'message': 'Authorized', 'token': token});
 })
 
 route.post('/signup', async (req, res) => {
     const data = req.body;
     if (verify(data)) return res.status(400).json({'error': 'Fields cannot be empty'});
-    const salt = bcrypt.genSaltSync(15);
     const hash = bcrypt.hashSync(data.pass, salt);
     const sql = `INSERT INTO users (name, username, passrowd) VALUES ('${data.name}', '${data.username}', '${hash}')`;
     
