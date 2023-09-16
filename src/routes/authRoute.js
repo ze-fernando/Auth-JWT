6fb88d65b99b30e782c5e14ca6d43c037fb2d210
ze-fernando <jfenginner935@gmail.com>
2023-09-15 23:58:19 -0300

    Insert DataBase 01

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,10 +1,9 @@
 const User = require('../domain/user');
-
 const route = require('express').Router();
-
 const secretKey = "z5*a5%s9@p0ºz44¨";
-
 const jwt = require('jsonwebtoken');
+const db = require('../connection/conn');
+const { json } = require('express');
 
 const users = [
     {name: "joao", username: "jao", pass: "joao123"},
@@ -24,12 +23,18 @@ route.post('/login', (req, res) => {
     res.json({token})
 })
 
-route.post('/signup', (req, res) => {
+route.post('/signup', async (req, res) => {
     const data = req.body;
-    const user = new User(data.name, data.username, data.pass);
-    users.push(user);
-    res.json({"message": 'ok'});
-    console.log(users)
+    const sql = `INSERT INTO users (name, username, passrowd) VALUES ('${data.name}', '${data.username}', '${data.pass}')`;
+    try {
+        const [query] = await db.execute(sql);
+    } catch (e) {
+        console.log('Error in Database', e);
+        return json({"error": 'Invalid Keys'})
+    }
+    finally{
+        return json({'message': 'Sucess'});
+    }
 })
 
 
